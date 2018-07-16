f76d13fbe49f6bca0bfad322fc037768c2ef0267
Marsel Gray <marselgray@gmail.com>
2018-07-16 15:52:35 -0400

    components and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import AppNavBar from './components/AppNavBar';
+import ShoppingList from './components/ShoppingList';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppNavBar/>
+        <ShoppingList/>
       </div>
     );
   }

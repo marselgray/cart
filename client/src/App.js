1fc96bd2cfe5d1097e2bc46cd2e0d8f4ff26dab0
Marsel Gray <marselgray@gmail.com>
2018-07-16 19:45:07 -0400

    added Redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,18 @@ import './App.css';
 import AppNavBar from './components/AppNavBar';
 import ShoppingList from './components/ShoppingList';
 
+import {Provider} from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavBar/>
-        <ShoppingList/>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavBar/>
+          <ShoppingList/>
+        </div>
+      </Provider>
     );
   }
 }

e0d9e7ded73d147d8a6feabdf29e0026a1a9e275
Marsel Gray <marselgray@gmail.com>
2018-07-18 21:20:09 -0400

    finished redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 import AppNavBar from './components/AppNavBar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+import {Container} from 'reactstrap';
 
 import {Provider} from 'react-redux';
 import store from './store';
@@ -14,7 +16,10 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <AppNavBar/>
-          <ShoppingList/>
+          <Container>
+            <ItemModal />
+            <ShoppingList/>
+          </Container>
         </div>
       </Provider>
     );

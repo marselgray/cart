0f4f60fa06fb169606b0703c265d62c3f6f83ff1
Marsel Gray <marselgray@gmail.com>
2018-07-19 13:38:52 -0400

    connected front and back end

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,22 +1,39 @@
-import {GET_ITEMS, ADD_ITEM, DELETE_ITEM} from './Types'
+import {GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING} from './Types'
+import axois from 'axios';
 
-export const getItems = () => {
-    return {
-        type: GET_ITEMS
-    };
+export const getItems = () => dispatch => {
+  dispatch(setItemsLoading());  
+  axois
+    .get('/api/items')
+    .then(res => dispatch({
+        type: GET_ITEMS,
+        payload: res.data
+        })
+    )
+};
+
+export const addItem = (item) => dispatch => {
+    axois
+        .post('/api/items', item)
+        .then(res => dispatch({
+            type: ADD_ITEM,
+            payload: res.data
+        })
+    )
 }
 
-export const deleteItem = (id) => {
-    return {
+export const deleteItem = (id) => dispatch => {
+    axois
+    .delete(`/api/items/${id}`)
+    .then(res => dispatch ({
         type: DELETE_ITEM,
         payload: id
-    };
+    }))
 }
 
-
-export const addItem = (item) => {
+export const setItemsLoading = () => {
     return {
-        type: ADD_ITEM,
-        payload: item
+        type: ITEMS_LOADING,
+        
     };
 }
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,30 +1,34 @@
 import uuid from 'uuid';
-import {GET_ITEMS, ADD_ITEM, DELETE_ITEM} from '../actions/Types';
+import {GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING} from '../actions/Types';
 
 const initialState = {
-   items: [
-        { id: uuid(), name: 'Eggs'},
-        { id: uuid(), name: 'Milk'},
-        { id: uuid(), name: 'Steak'},
-        { id: uuid(), name: 'Apples'}
-    ]
+   items: [],
+   loading: false,
+
 }
 
 export default function(state = initialState, action){
     switch(action.type){
         case GET_ITEMS: 
             return {
-                ...state
+                ...state,
+                items: action.payload,
+                loading: false
             };
         case DELETE_ITEM:
             return {
                 ...state,
-                items: state.items.filter(item => item.id !==action.payload)
+                items: state.items.filter(item => item._id !==action.payload)
             }
         case ADD_ITEM:
             return {
                 ...state,
                 items: [action.payload, ...state.items]
+            };
+        case ITEMS_LOADING:
+            return {
+                ...state,
+                laoding: true
             }
         default: 
         return state;
